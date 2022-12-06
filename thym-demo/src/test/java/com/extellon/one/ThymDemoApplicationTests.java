bce81f95b5348cc897934cacb98d290383c60991
atishdixit <atishdixit26@gmail.com>
2022-12-06 09:39:34 +0530

    Initial commit

---
+++ thym-demo/src/test/java/com/extellon/one/ThymDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extellon.one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
